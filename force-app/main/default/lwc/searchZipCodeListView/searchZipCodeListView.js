8d1bb59b0fa40440d9b6854ef3f9ccd2e5118ae0
Edgar Santillana <edgar.santillanaa@gmail.com>
2024-07-31 18:26:33 -0600

    Created Apex Classes and LWC

---
+++ force-app/main/default/lwc/searchZipCodeListView/searchZipCodeListView.js
@@ -0,0 +1,17 @@
+/**
+ * @description       : 
+ * @author            : Edgar Santillana
+ * @group             : 
+ * @last modified on  : 07-30-2024
+ * @last modified by  : Edgar Santillana 
+ * Modifications Log
+ * Ver   Date         Author             Modification
+ * 1.0   07-30-2024   Edgar Santillana   Initial Version
+**/
+import { LightningElement, api } from 'lwc';
+
+export default class SearchZipCodeListView extends LightningElement {
+
+    @api location;
+    
+}
